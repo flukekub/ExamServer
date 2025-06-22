2bd670b071536d1dfe91084f09cb5c8e0e32a4ff
Pattarapon Noonun <fluk3874@gmail.com>
2025-06-22 17:37:28 +0700

    add more config

--- index.js
+++ index.js
@@ -27,5 +27,5 @@ app.use('/api/v1/subjects', subjectRoutes);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is running in ${process.env.NODE_ENV} on ${process.env.HOST}:${process.env.PORT}`);
 });
